09b455da57b479444b801eecfe39932c6057ad9f
saven <SavenkoNikita@yandex.com>
2024-01-11 21:09:26 +0300

    homework 1

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -1,19 +1,19 @@
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
+    # впишите правильные адреса страниц, используя функцию `reverse`
     pages = {
-        'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Главная страница': reverse(''),
+        'Показать текущее время': reverse('current_time/'),
+        'Показать содержимое рабочей директории': reverse('workdir/')
     }
     
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
+    # context и параметры render менять не нужно подробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
@@ -21,15 +21,13 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
+    # обратите внимание – здесь HTML шаблона нет, возвращается просто текст
+    current_time = datetime.datetime.now().time().strftime('%H:%M')
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
+    # по аналогии с `time_view`, напишите код, который возвращает список файлов в рабочей директории
+
     raise NotImplemented
